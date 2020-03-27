ea763192c6b0485f2b632822c5619fd4964ac3c9
Pedro Pires <ph_pires@poli.ufrj.br>
2020-03-26 23:04:55 -0300

    Application back-end

---
+++ back-end/src/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const app = express(); // instanciando a aplicação
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+/**
+ * Rota / Recurso
+ */
+
+ /**
+  * Métodos Http:
+  * 
+  * GET: Buscar/listar informação do back-end
+  * POST: Criar informação no back-end
+  * PUT: Alterar uma informação no back-end
+  * DELETE: Deletar uma informação no back-end
+  */
+
+  /**
+   * Tipos de Parâmetros
+   * 
+   * Query Params: Parâmetros nomeados enviados na rota após '?' (filtros, paginação)
+   * Route Params: Parâmaetros utilizados para identificar rescursos.
+   * Body Params: Corpo da requisição, utilizado para criar ou alterar recursos
+   */
+  /**
+   * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+   * NoSQL: MongoDB, CouchDB, etc
+   */
+  /**
+   * Driver: SELECT * FROM users
+   * Query Builder: tables('users').select('*').where()
+   */
+
+
